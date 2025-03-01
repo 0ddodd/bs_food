98e7d87aa82dfffe089f9ec8f13747017af7fc43
0ddodd <hegoeson@gmail.com>
2025-03-02 01:21:40 +0900

    onmounted 하나로 합침

--- client/src/components/MainVue.vue
+++ client/src/components/MainVue.vue
@@ -1,16 +1,17 @@
 <template>
-    <div>
-        <div class="container text-center">
-            <div class="row">
-                <div class="col-4" v-for="info, i in foodImgInfo" :key="i">
-                    <div >
-                        <p>{{getFoodName[info.MENU_ID]}}</p>
+    <div class="container text-center">
+        <div class="row">
+            <div class="col-4" v-for="info, i in foodImgInfo" :key="i">
+                <div class="card mb-3">
+                    <img
+                        :src="info.FOOD_IMG_URL"
+                        class="img-fluid"
+                        style="width: 100%; height: 300px;"    
+                    />
+                    <div class="card-body">
+                        <p v-if="getFoodName[info.MENU_ID]">{{getFoodName[info.MENU_ID]}}</p>
                         <p>{{info.RSTR_NM}}</p>
                         <p>{{info.AREA_NM}}</p>
-                        <img
-                            :src="info.FOOD_IMG_URL"
-                            style="width: 300px; height: 300px;"    
-                        />
                     </div>
                 </div>
             </div>
@@ -26,16 +27,20 @@
     const foodImgInfo = ref([]);
     
     onMounted(async () => {
-        const result = await axios.get(`${process.env.VUE_APP_API_URL}/menus/info`);
-        console.log(result.data.body);
-        foodInfo.value = result.data.body;
-    });
 
-    onMounted(async() => {
-        const result = await axios.get(`${process.env.VUE_APP_API_URL}/menus/img`);
-        console.log(result.data.body);
-        foodImgInfo.value = result.data.body;
-    })
+        const [infoResult, imgResult] = await Promise.all([
+            await axios.get(`${process.env.VUE_APP_API_URL}/menus/info`),
+            await axios.get(`${process.env.VUE_APP_API_URL}/menus/img`)
+        ])
+
+        foodInfo.value = infoResult.data.body;
+        foodImgInfo.value = imgResult.data.body.reduce((acc, curr) => {
+            if (!acc.some(item => item.MENU_ID === curr.MENU_ID)) {
+                acc.push(curr);
+            }
+            return acc;
+        }, []);
+    });
 
     const getFoodName = computed(() => {
         if (foodInfo.value.length === 0) return {}
